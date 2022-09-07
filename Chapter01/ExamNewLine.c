04eecc81cbdc6e9239fd27b940b51d84d82805a1
chocho77 <chavmom@gmail.com>
2022-09-07 05:13:35 +0300

    Create ExamNewLine.c

---
+++ Chapter01/ExamNewLine.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    /* escape sequence = character combination consisting of a backslash \
+                         followed by a letter or combination of digits.
+                         They specify actions within a line or string of text.
+                         \n = newline
+                         \t = tab
+
+    */
+
+    printf("\"I like Pizza\" - Abraham Lincoln probably");
+
+    return 0;
+}
