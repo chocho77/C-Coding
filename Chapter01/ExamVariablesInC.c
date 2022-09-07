9e4ddcf33d503109bdafc61145d7b1f5b5f8c300
chocho77 <chavmom@gmail.com>
2022-09-07 05:24:29 +0300

    Create ExamVariablesInC.c

---
+++ Chapter01/ExamVariablesInC.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // variable = Allocated space in memory to store a value.
+    //            We refer to a variable's name to access the stored value.
+    //            That variable now behaves as if it was the value it contains.
+    //            BUT we need to declare what type of data we are storing.
+
+    return 0;
+}
