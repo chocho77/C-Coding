da7977f28f8fe85a9ed52a8cdecf0e2fe99f0766
chocho77 <chavmom@gmail.com>
2022-09-07 05:45:31 +0300

    Update ExamVariablesInC.c

--- Chapter01/ExamVariablesInC.c
+++ Chapter01/ExamVariablesInC.c
@@ -8,10 +8,15 @@ int main()
     //            That variable now behaves as if it was the value it contains.
     //            BUT we need to declare what type of data we are storing.
 
-    int x;   // declaration
-    x = 123; // initialization
-    
+    int x;       // declaration
+    x = 123;     // initialization
+    int y = 321; // declaration + initialization
+
+    int age = 21; // integer
+
     printf("x = %d\n", x);
+    printf("y = %d\n", y);
+    printf("age = %d\n", age);
 
     return 0;
 }
